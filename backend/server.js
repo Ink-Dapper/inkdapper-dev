ddb0d01b1fe919498d18ca39f0cbdeceaa57ada6
root <root@srv693737.hstgr.cloud>
2025-05-18 19:55:11 +0000

    Your message about changes to server.js may-19

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,28 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+const allowedOrigins = [
+  'https://inkdapper.com',
+  'https://www.inkdapper.com',
+  'https://admin.inkdapper.com'
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin); // Set only one allowed origin dynamically
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, token");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json());
 
 //api endpoints
